a54b7da0eda596b597c0647580f6085256008f5c
SweatyKitty <nigensey@gmail.com>
2024-10-20 17:14:00 +0300

    18/10

--- main.py
+++ main.py
@@ -4,13 +4,26 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#task1
+#1st program
+print(type(0.5)) #Проверка сработает ли число типа float, написанное через запятую
+print((9**0.5)*5)
+
+#task2
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+#task3
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+#task4
+#4st program
+print('123.456')
+print(int(((float('123.456')*1000)-123056)/100))
+print(int(((float('123.456')*10)-1230)//1)) #вариант, предложенный в алгоритме задачи
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('world')
-print("Hello world")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
