63c6f718ece7c40bf50882d8608a69b852f6c951
SweatyKitty <nigensey@gmail.com>
2024-10-24 19:22:15 +0300

    Create module_1_6.py

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+Number_of_complete_tasks =12
+amount_of_spended_hours= 1.5
+Name_of_course='Python'
+time_for_one_task=amount_of_spended_hours/Number_of_complete_tasks
+print('Курс:',Name_of_course,',всего задач:',Number_of_complete_tasks,',затраченных часов:',amount_of_spended_hours,',среднее время выполнения:',time_for_one_task*60,'минут.')
