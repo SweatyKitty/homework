395550269c79b6d8e4a3c36b32268aa3cf3552a2
SweatyKitty <nigensey@gmail.com>
2024-10-27 20:32:17 +0300

    Create module_1_4.py

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string=input('Введите произвольный текст: ')#Task1
+print(len(my_string))#Task2
+print(my_string.upper())#Task3.1
+print(my_string.lower())#Task3.2
+print(my_string.replace(' ',''))#Task3.3
+print(my_string[0])#Task3.4
+print(my_string[-1])#Task3.5
