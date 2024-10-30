b85d4dcf2bc0ee0f53be28e538d81bc1f80a17f8
SweatyKitty <nigensey@gmail.com>
2024-10-30 19:54:51 +0300

    Create module_1_5.py

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var=1,True,'data'#Task2
+print('Immutable tuple: ',immutable_var)
+#immutable_var[0]=3 потому что не изманяемый тип, однако может содержать изменяемые данные task3
+mutable_list=[1,False,'prompt'] #task4
+mutable_list[1]=True
+mutable_list[0]=38
+print('Mutable list: ',mutable_list)
