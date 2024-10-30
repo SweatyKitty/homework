dac486261cadceda0df560a7a17ffad394126321
SweatyKitty <nigensey@gmail.com>
2024-10-30 23:35:41 +0300

    Update 30.10

--- module_1_6.py
+++ module_1_6.py
@@ -1,5 +1,18 @@
-Number_of_complete_tasks =12
-amount_of_spended_hours= 1.5
-Name_of_course='Python'
-time_for_one_task=amount_of_spended_hours/Number_of_complete_tasks
-print('Курс:',Name_of_course,',всего задач:',Number_of_complete_tasks,',затраченных часов:',amount_of_spended_hours,',среднее время выполнения:',time_for_one_task*60,'минут.')
+#task2
+from tkinter.ttk import Treeview
+
+my_dict={'Yaroslav':1998,'Yulia':2000,}
+print('Dict: ',my_dict)
+print('Existing value: ',my_dict['Yaroslav'])
+print('Not existing value: ',my_dict.get('Jaroslav'))
+my_dict.update({'Mihail':1999,'Nikolya':2001})
+a=my_dict.pop('Yaroslav')
+print('Deleted value: ',a)
+print('Modified dictionary: ',my_dict)
+#task3
+my_set={1,2,3,3,3,4,5,5,True,False,True,'Shade','Shade'}
+print('Set:',my_set)
+my_set.add(6)
+my_set.add(7)
+my_set.discard(3)
+print('Modified set:',my_set)
