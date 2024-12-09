e3ff269e2a3abded6dc8d746268bf697bfba9ee4
SweatyKitty <nigensey@gmail.com>
2024-12-09 15:33:40 +0300

    module_3_hard_complete

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-b={'Антон':123, 'Андон':456, "Дуралей":789}
-a=[*b]
-print(a)
-for i in range(len(b)):
-    print(b[a[i]])
-
-
-print(type({'Антон':123, 'Андон':456, "Дуралей":789}))
-f={123,456,789}
-print(*f)
-print(isinstance(f,int))
-for i is set:
-
+a={(2, 'Urban', ('Urban2', 35))}
+b=list(a)
+print(b)
