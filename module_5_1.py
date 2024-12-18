4bb14124982b03290fc27ffda465736553e603f0
SweatyKitty <nigensey@gmail.com>
2024-12-18 21:17:44 +0300

    5_3_homework

--- module_5_1.py
+++ module_5_1.py
@@ -21,55 +21,104 @@ class House:
     
     
     def __eq__(self,other):
-        if self.number_of_floors == other.number_of_floors:
-            return True
+        if isinstance(other,int):
+            return self.number_of_floors==other
+        elif isinstance(other,House):
+            return self.number_of_floors == other.number_of_floors
         else:
-            return False
+            print('Ne tot class- ', type(other), other)
     def __it__(self,other):
-        if self.number_of_floors < other.number_of_floors:
-            return True
+        if isinstance(other,int):
+            return self.number_of_floors<other
+        elif isinstance(other,House):
+            return self.number_of_floors < other.number_of_floors
         else:
-            return False
+            print('Ne tot class- ', type(other), other)
     def __le__(self,other):
-        if self.number_of_floors <= other.number_of_floors:
-            return True
+        if isinstance(other,int):
+            return self.number_of_floors<=other
+        elif isinstance(other,House):
+            return self.number_of_floors <= other.number_of_floors
         else:
-            return False
+            print('Ne tot class- ', type(other), other)
     def __gt__(self,other):
-        if self.number_of_floors>other.number_of_floors:
-            return True
+        if isinstance(other,int):
+            return self.number_of_floors>other
+        elif isinstance(other,House):
+            return self.number_of_floors>other.number_of_floors
         else:
-            return False
+            print('Ne tot class- ', type(other), other)
     def __ge__(self,other):
-        if self.number_of_floors>=other.number_of_floors:
-            return True
+        if isinstance(other,int):
+            return self.number_of_floors>=other
+        elif isinstance(other,House):
+            return self.number_of_floors>=other.number_of_floors
         else:
-            return False
+            print('Ne tot class- ', type(other), other)
     def __ne__(self,other):
-        if self.number_of_floors!=other.number_of_floors:
-            return True
+        if isinstance(other,int):
+            return self.number_of_floors!=other
+        elif isinstance(other,House):
+            return self.number_of_floors!=other.number_of_floors
         else:
-            return False
+            print('Ne tot class- ', type(other), other)
     def __add__(self,value):
-        self.number_of_floors+=value
-        return self.number_of_floors
+        if isinstance(value,int):
+            self.number_of_floors += value
+            return self
+        elif isinstance(value,House):
+            self.number_of_floors += value.number_of_floors
+            return self
+        else:
+            print('Ne tot class- ', type(value), value)
     def __radd__(self,value):
-        return add(self,value)
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+            return self
+        else:
+            print('Ne tot class- ', type(value), value)
     def __iadd__(self,value):
-        return add(self,value)
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+            return self
+        else:
+            print('Ne tot class- ', type(value), value)
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-
-h1.go_to(0)
-h2.go_to(2)
-
-# Следующее задание module_5_2 тоже выполнено здесь
+# h1 = House('ЖК Горский', 18)
+# h2 = House('Домик в деревне', 2)
+#
+# h1.go_to(0)
+# h2.go_to(2)
+#
+# # Следующее задание module_5_2 тоже выполнено здесь
 
 h3 = House('ЖК Эльбрус', 10)
 h4 = House('ЖК Акация', 20)
 
-print(h3.__str__())
-print(len(h3))
-print(str(h3))
-print(len(h4))
+# print(h3.__str__())
+# print(len(h3))
+# print(str(h3))
+# print(len(h4))
+
+print('module_5_3:')
+print(h3)
+print(h4)
+print(h3 == h4) # __eq__
+h3 = h3 + 10 # __add__
+print(h3)
+print(h3 == h4)
+h3 += 10 # __iadd__
+print(h3)
+h4 = 10 + h4 # __radd__
+print(h4)
+print(h3 > h4) # __gt__
+print(h3 >= h4) # __ge__
+print(h3 < h4) # __lt__
+print(h3 <= h4) # __le__
+print(h3 != h4) # __ne__
